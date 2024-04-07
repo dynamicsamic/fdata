3ce5f5eb6ec71658cadd4a797adeff6420b5603b
dynamicsamic <mirabyansr@gmail.com>
2024-04-07 23:52:49 +0300

    refactor: update `show_circuits` to iclude num races for each circiut

--- stats/views.py
+++ stats/views.py
@@ -1,3 +1,17 @@
+from django.db.models import Count
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Circuit
+
+
+def show_circuits(request: HttpRequest):
+    qs = Circuit.objects.annotate(num_races=Count("races"))
+    theaders = Circuit.get_fieldnames()
+    theaders.append("num_races")
+
+    return render(
+        request,
+        "stats/circuits.html",
+        {"circuits": qs, "theaders": theaders},
+    )
