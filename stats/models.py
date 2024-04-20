e4b08102eef3e216d23ddb59820eb92df9957cda
dynamicsamic <mirabyansr@gmail.com>
2024-04-20 23:43:30 +0300

    feat: complete models set

--- stats/models.py
+++ stats/models.py
@@ -16,6 +16,9 @@ class AbstractBaseModel(models.Model):
     class Meta:
         abstract = True
 
+    def _str(self, s: str) -> str:
+        return f"{self.__class__.__name__}({s})"
+
 
 class Circuit(AbstractBaseModel):
     short_name = models.CharField(max_length=128)
@@ -25,10 +28,39 @@ class Circuit(AbstractBaseModel):
     latitude = models.FloatField()
     longitude = models.FloatField()
     altitude = models.SmallIntegerField(blank=True)
-    url = models.URLField(max_length=256)
+    url = models.URLField()
 
     def __str__(self) -> str:
-        return f"{self.short_name}"
+        return self._str(self.short_name)
+
+
+class Driver(AbstractBaseModel):
+    short_name = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    birthdate = models.DateField()
+    nationality = models.CharField(max_length=256)
+    url = models.URLField()
+
+    def __str__(self) -> str:
+        return self._str(f"{self.first_name} {self.last_name}")
+
+
+class Constructor(AbstractBaseModel):
+    short_name = models.CharField(max_length=256)
+    name = models.CharField(max_length=500)
+    nationality = models.CharField(max_length=256)
+    url = models.URLField()
+
+    def __str__(self) -> str:
+        return self._str(self.short_name)
+
+
+class ResultSatus(AbstractBaseModel):
+    name = models.CharField(max_length=256)
+
+    def __str__(self) -> str:
+        return self._str(self.name)
 
 
 class Race(AbstractBaseModel):
@@ -39,24 +71,12 @@ class Race(AbstractBaseModel):
     round = models.PositiveSmallIntegerField()
     name = models.CharField(max_length=256)
     date = models.DateField()
-    url = models.URLField(max_length=256)
+    url = models.URLField()
 
     def __str__(self) -> str:
-        return f"{self.name} {self.year}"
+        return self._str(f"{self.name}, {self.year}")
 
 
-class Driver(AbstractBaseModel):
-    short_name = models.CharField(max_length=128)
-    first_name = models.CharField(max_length=256)
-    last_name = models.CharField(max_length=256)
-    birthdate = models.DateField()
-    nationality = models.CharField(max_length=256)
-    url = models.URLField(max_length=256)
-
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
-
-      
 class DriverStanding(AbstractBaseModel):
     race = models.ForeignKey(
         Race, on_delete=models.PROTECT, related_name="driver_standings"
@@ -67,17 +87,120 @@ class DriverStanding(AbstractBaseModel):
     points = models.PositiveSmallIntegerField()
     position = models.PositiveSmallIntegerField()
     wins = models.PositiveSmallIntegerField()
-    
+
+    def __str__(self) -> str:
+        return self._str(
+            f"Race:{self.race_id}, Driver:{self.driver_id}, "
+            f"Pos:{self.position}, Points:{self.points}"
+        )
+
+
+class ConstructorStanding(AbstractBaseModel):
+    race = models.ForeignKey(
+        Race, on_delete=models.PROTECT, related_name="constr_standings"
+    )
+    constructor = models.ForeignKey(
+        Constructor, on_delete=models.PROTECT, related_name="standings"
+    )
+    points = models.PositiveSmallIntegerField()
+    position = models.PositiveSmallIntegerField()
+    wins = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return self._str(
+            f"Race:{self.race_id}, Driver:{self.constructor_id}, "
+            f"Pos:{self.position}, Points:{self.points}"
+        )
+
+
+class Result(AbstractBaseModel):
+    race = models.ForeignKey(
+        Race, on_delete=models.PROTECT, related_name="results"
+    )
+    driver = models.ForeignKey(
+        Driver, on_delete=models.PROTECT, related_name="race_results"
+    )
+    constructor = models.ForeignKey(
+        Constructor, on_delete=models.PROTECT, related_name="race_results"
+    )
+    grid_position = models.ForeignKey(
+        DriverStanding, on_delete=models.PROTECT, related_name="race_results"
+    )
+    status = models.ForeignKey(
+        ResultSatus, on_delete=models.PROTECT, related_name="race_results"
+    )
+    car_number = models.PositiveSmallIntegerField()
+    final_position = models.PositiveSmallIntegerField()
+    points = models.PositiveSmallIntegerField()
+    laps = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return self._str(
+            f"Race:{self.race_id}, Driver:{self.driver_id} "
+            f"Constructor:{self.constructor_id}, Status:{self.status_id}"
+            f"Position:{self.final_position}, Points:{self.points}"
+            f"Laps:{self.laps}"
+        )
+
+
+class Qualifying(AbstractBaseModel):
+    race = models.ForeignKey(
+        Race, on_delete=models.PROTECT, related_name="qualifyings"
+    )
+    driver = models.ForeignKey(
+        Driver, on_delete=models.PROTECT, related_name="qualifyings"
+    )
+    constructor = models.ForeignKey(
+        Constructor, on_delete=models.PROTECT, related_name="qualifyings"
+    )
+    car_number = models.PositiveSmallIntegerField()
+    position = models.PositiveSmallIntegerField()
+    q1 = models.TimeField(null=True)
+    q2 = models.TimeField(null=True)
+    q3 = models.TimeField(null=True)
+
+    def __str__(self) -> str:
+        return self._str(
+            f"Race:{self.race_id}, Driver:{self.driver_id}, "
+            f"Costructor:{self.constructor_id}, Pos:{self.position}, "
+            f"Time:{self.q3 or self.q2 or self.q1}"
+        )
+
 
 class LapTime(AbstractBaseModel):
     race = models.ForeignKey(
         Race, on_delete=models.PROTECT, related_name="lap_times"
-    ),
+    )
     driver = models.ForeignKey(
         Driver, on_delete=models.PROTECT, related_name="lap_times"
-    ),
-    lap_no = models.PositiveSmallIntegerField(),
-    position = models.PositiveSmallIntegerField(),
-    time = models.TimeField(),
+    )
+    lap = models.PositiveSmallIntegerField()
+    position = models.PositiveIntegerField()
+    time = models.TimeField()
     time_ms = models.PositiveSmallIntegerField()
-    
+
+    def __str__(self) -> str:
+        return self._str(
+            f"Race:{self.race_id}, Driver:{self.driver_id}, Time:{self.time}, "
+            f"Lap no:{self.lap_no}, Pos:{self.position}"
+        )
+
+
+class PitStopTime(AbstractBaseModel):
+    race = models.ForeignKey(
+        Race, on_delete=models.PROTECT, related_name="pitstop_times"
+    )
+    driver = models.ForeignKey(
+        Driver, on_delete=models.PROTECT, related_name="pitstop_times"
+    )
+    stop_number = models.PositiveSmallIntegerField()
+    lap = models.PositiveSmallIntegerField()
+    time = models.TimeField()
+    duration = models.FloatField()
+    duration_ms = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return self._str(
+            f"Race:{self.race_id}, Driver:{self.driver_id}, Lap:{self.lap}, "
+            f"Stop no:{self.stop_number}, Duration:{self.duration}"
+        )
